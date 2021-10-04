a3cd623d083f3700c05e073060ed2649f899c192
karinagaspars <91804827+karinagaspars@users.noreply.github.com>
2021-10-04 17:17:25 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <meta charset="utf-8">
+    <head>
+      <img src="tumb.jpeg" height="50%" width="100%">
+    </head>
+    <title>IMPACTOS AMBIENTAIS</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+  <body class="tela-green" >
+          IMPACTOS AMBIENTAIS CAUSADOS PELO HOMEM
+          <h2 class="tema1">Principais tipos de impactos ambientais causados pelo homem </h2>
+           
+  </body>
+</html>
