e1d0d47d6ea3fad884606bf79aea93ccf2686ec0
karinagaspars <91804827+karinagaspars@users.noreply.github.com>
2021-10-04 19:01:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 <html lang="pt-br">
     <meta charset="utf-8">
     <head>
-      <img src="tumb.jpeg" height="50%" width="100%">
-    </head>
-    <title>IMPACTOS AMBIENTAIS</title>
+      <title>IMPACTOS AMBIENTAIS</title>
+      <img src="tumb.jpeg" height="50%" width="100%"/>
+      <h1>Impactos Ambientais</h1>
+
     <link rel="stylesheet" href="style.css" type="text/css">
+    <meta name= "viwport" content="width=device-wigth, initial=scale=1.0, maximus-scale=1.0">
     </head>
-  <body class="tela-green" >
-          IMPACTOS AMBIENTAIS CAUSADOS PELO HOMEM
-          <h2 class="tema1">Principais tipos de impactos ambientais causados pelo homem </h2>
+  <body class="tela-green">
+           
            
   </body>
 </html>
